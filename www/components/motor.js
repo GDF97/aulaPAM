cde0a43d905845b9ac2c00a08cd205df7a6083c6
GDF97 <pedroupandofacil@hotmail.com>
2023-03-07 00:27:13 +0900

    Regfra de tres

--- www/components/motor.js
+++ www/components/motor.js
@@ -1,3 +1,14 @@
+
+
+
 function HelloWorld(){
-    alert("Hello World!");
+
+var Valor1 = document.getElementById('V1').value;
+var Valor2 = document.getElementById('V2').value;
+var Valor3 = document.getElementById('V3').value;
+var Valor4 = ((Valor2*Valor3)/Valor1);
+
+var Resultado = document.getElementById('header');
+Resultado.innerText = "O valor é = " + Valor4;
+
 }
