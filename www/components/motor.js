50baa4516e2d54e7044e178130624540cd190b07
GDF97 <pedroupandofacil@hotmail.com>
2023-03-13 09:28:10 +0900

    Estilização do calculador da regra de 3

--- www/components/motor.js
+++ www/components/motor.js
@@ -1,14 +1,12 @@
+    
+const btn = document.getElementById('btn');
 
-
-
-function HelloWorld(){
-
-var Valor1 = document.getElementById('V1').value;
-var Valor2 = document.getElementById('V2').value;
-var Valor3 = document.getElementById('V3').value;
-var Valor4 = ((Valor2*Valor3)/Valor1);
-
-var Resultado = document.getElementById('header');
-Resultado.innerText = "O valor é = " + Valor4;
-
-}
+btn.addEventListener('click', function(){
+    var Valor1 = document.getElementById('V1').value;
+    var Valor2 = document.getElementById('V2').value;
+    var Valor3 = document.getElementById('V3').value;
+    var Valor4 = ((Valor2*Valor3)/Valor1);
+    
+    var Resultado = document.getElementById('Res');
+    Resultado.innerText += " = " + Valor4;
+})
